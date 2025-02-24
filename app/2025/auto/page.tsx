b5a3f746e3411a7f22b95a8c8423d51fba22747c
MobSlicer152 <brambleclaw1414@gmail.com>
2025-02-24 12:33:22 -0500

    Make auto match teleop

--- app/2025/auto/page.tsx
+++ app/2025/auto/page.tsx
@@ -14,10 +14,14 @@ export default function AutoPage() {
         <p className="text-lg text-center">Auto</p>
       </div>
       <div className="flex flex-col m-4 w-full items-center">
-        <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl1} onChange={change => context.getData().autoCoralLvl1 += change}>Level 1 Coral</ScoreCounter>
-        <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl2} onChange={change => context.getData().autoCoralLvl2 += change}>Level 2 Coral</ScoreCounter>
-        <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl3} onChange={change => context.getData().autoCoralLvl3 += change}>Level 3 Coral</ScoreCounter>
-        <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl4} onChange={change => context.getData().autoCoralLvl4 += change}>Level 4 Coral</ScoreCounter>
+        <div className="flex flex-row">
+          <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl1} onChange={change => context.getData().autoCoralLvl1 += change}>Level 1 Coral</ScoreCounter>
+          <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl2} onChange={change => context.getData().autoCoralLvl2 += change}>Level 2 Coral</ScoreCounter>
+        </div>
+        <div className="flex flex-row">
+          <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl3} onChange={change => context.getData().autoCoralLvl3 += change}>Level 3 Coral</ScoreCounter>
+          <ScoreCounter className="m-4" initialValue={context.getData().autoCoralLvl4} onChange={change => context.getData().autoCoralLvl4 += change}>Level 4 Coral</ScoreCounter>
+        </div>
         <ScoreCounter className="m-4" initialValue={context.getData().autoBarge} onChange={change => context.getData().autoBarge += change}>Barge</ScoreCounter>
         <Checkbox className="m-4" value={context.getData().autoMobility} onChange={checked => context.getData().autoMobility = checked}>Mobility</Checkbox>
       </div>
